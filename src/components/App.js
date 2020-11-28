eaf7ea3700ea15bc506e93b88c33c7c9adeb0de0
sarafkalpak <sarafkalpak@gmail.com>
2020-11-28 17:42:00 +0530

    nw1

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
-import Heading from './components/Heading';
-import InputQuery from './components/InputQuery';
-import SubHeading from './components/SubHeading';
-import SubmitButton from './components/SubmitButton';
+import Heading from './Heading';
+import InputQuery from './InputQuery';
+import SubHeading from './SubHeading';
+import SubmitButton from './SubmitButton';
 
 const App = () => {
   return (
